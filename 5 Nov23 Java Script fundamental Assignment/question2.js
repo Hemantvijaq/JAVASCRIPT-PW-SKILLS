7d01337102fb690e17106db2079d5bef65164e53
Hemantvijaq <147089473+Hemantvijaq@users.noreply.github.com>
2024-02-02 15:15:50 +0530

    Add files via upload

---
+++ 5 Nov23 Java Script fundamental Assignment/question2.js
@@ -0,0 +1,5 @@
+//Comma operator allows us to evaluate multiple expressions in a single statement like declaring multipl variables in a single statement separating them with the comma operator.
+
+let x = 15;
+(x++, x+= 2,x*= 5);
+console.log(x);
---
+++ 5 Nov23 Java Script fundamental Assignment/question4.js
@@ -0,0 +1,3 @@
+let age = 21;
+let votinglimit= age >=18 ? 'Yes': 'No';
+console.log('can fit criteria', votinglimit);
